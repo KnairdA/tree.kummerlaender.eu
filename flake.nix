436c6b498b3f8f64f725e1cb79696df4dfab5313
Adrian Kummerlaender <adrian@kummerlaender.eu>
2021-09-05 14:28:40 +0200

    Flakeify

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "static site generator for tree.kummerlaender.eu";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.05;
+    pkgs-personal.url = git+https://code.kummerlaender.eu/pkgs;
+  };
+
+  outputs = { self, nixpkgs, pkgs-personal, ... }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShell.x86_64-linux = pkgs.mkShell {
+      buildInputs = [
+        pkgs.pandoc
+        pkgs.highlight
+        pkgs-personal.katex-wrapper
+        pkgs-personal.make-xslt
+      ];
+    };
+
+    generate = content: pkgs.stdenv.mkDerivation {
+      name = "tree.kummerlaender.eu";
+
+      src = ./.;
+
+      LANG = "en_US.UTF-8";
+
+      buildInputs = [
+        pkgs.pandoc
+        pkgs.highlight
+        pkgs-personal.katex-wrapper
+        pkgs-personal.make-xslt
+      ];
+
+      installPhase = ''
+        cp -r ${content} source/00_content
+        make-xslt
+        mkdir $out
+        cp -Lr target/99_result/* $out
+      '';
+    };
+  };
+}
